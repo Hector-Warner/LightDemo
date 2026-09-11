730503a2e1076af6a7a1b3d73e400569cef658f5
Hector Warner <hxw363@student.bham.ac.uk>
2026-09-11 16:36:00 +0100

    Added Monster spawn points

---
+++ Assets/Scripts/MonsterSpawner.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    GameObject Player;
+    bool spawned = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    enum MonsterType
+    {
+        FlowerCreature,
+        Giant
+    }
+
+    public int SpawnAmount;
+
+    [SerializeField]
+    MonsterType monsterType = new MonsterType();
+
+    public GameObject MonsterPrefab;
+
+    void Start()
+    {
+        PlayerController playerScript = FindFirstObjectByType<PlayerController>();
+        if (playerScript != null)
+        {
+            Player = playerScript.gameObject;
+        }
+        Debug.Log("Loaded");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spawned)
+        {
+            if (calculateDistance(Player.transform.position,transform.position) < 30)
+            {
+                Debug.Log("Spawning");
+                SpawnMonsters();
+            }
+        }
+    }
+
+    public float calculateDistance(Vector2 startCoord, Vector2 endCoord)
+    {
+        float distance = Mathf.Abs(startCoord.x - endCoord.x) + Mathf.Abs(startCoord.y - endCoord.y);
+        Debug.Log(distance);
+        return distance;
+    }
+
+    void SpawnMonsters()
+    {
+        for (int i = 0; i < SpawnAmount; i++)
+        {
+            Instantiate(MonsterPrefab, transform.position, transform.rotation);
+        }
+        spawned = true;
+    }
+}
